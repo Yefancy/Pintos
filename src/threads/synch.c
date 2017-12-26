940d424e9436b55683458a971dd365a551375012
haoyu6427 <1043921295@qq.com>
2017-12-26 13:06:43 +0800

    修改cond_signal
    debug

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -368,8 +368,11 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (lock_held_by_current_thread (lock));
 
   if (!list_empty (&cond->waiters))
+  {
+    list_sort (&cond->waiters, sema_compare_priority, NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
 }
 
 bool
