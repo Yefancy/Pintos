d593f960a3e458d4fcaf11b68249b359e11bd1d8
JackLfz <471283649@qq.com>
2017-12-27 13:14:09 +0800

    完善
    从所有的按序插入 修正为排序在取

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -581,7 +581,10 @@ next_thread_to_run (void)
   if (list_empty (&ready_list))
     return idle_thread;
   else
+  {
+    list_sort(&ready_list,thread_compare_priority,NULL);
     return list_entry (list_pop_front (&ready_list), struct thread, elem);
+  }
 }
 
 /* Completes a thread switch by activating the new thread's page
